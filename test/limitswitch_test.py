7b316287f703c3ebdac3fa616cbf21c1f52463b5
JuniorThap <Thapanawatsuriyaroj@gmail.com>
2025-12-16 07:50:37 +0700

    fix motion_test

--- test/limitswitch_test.py
+++ test/limitswitch_test.py
@@ -1,24 +1,42 @@
 import pigpio
 import time
 
-from src.hardware_config import LIMIT_SWITCH_PIN
+from src.hardware_config import (
+    LIMIT_SWITCH_PIN,
+    HOMING_X_LIMIT_SWITCH_PIN,
+    HOMING_Y_LIMIT_SWITCH_PIN
+)
 
 pi = pigpio.pi()
 if not pi.connected:
     raise RuntimeError("pigpio daemon not running. Run: sudo pigpiod")
 
-# Configure pin
-pi.set_mode(LIMIT_SWITCH_PIN, pigpio.INPUT)
-pi.set_pull_up_down(LIMIT_SWITCH_PIN, pigpio.PUD_UP)
+# Configure all pins as INPUT with internal pull-up
+for pin in [
+    LIMIT_SWITCH_PIN,
+    HOMING_X_LIMIT_SWITCH_PIN,
+    HOMING_Y_LIMIT_SWITCH_PIN
+]:
+    pi.set_mode(pin, pigpio.INPUT)
+    pi.set_pull_up_down(pin, pigpio.PUD_UP)
 
-print("Reading limit switch (Ctrl+C to exit)")
-print("1 = NOT pressed, 0 = PRESSED")
+print("Reading limit switches (Ctrl+C to exit)")
+print("1 = NOT pressed, 0 = PRESSED\n")
 
 try:
     while True:
-        val = pi.read(LIMIT_SWITCH_PIN)
-        print(val)
-        time.sleep(0.1)   # read every 100 ms
+        z_val = pi.read(LIMIT_SWITCH_PIN)
+        x_val = pi.read(HOMING_X_LIMIT_SWITCH_PIN)
+        y_val = pi.read(HOMING_Y_LIMIT_SWITCH_PIN)
+
+        print(
+            f"Z_AXIS: {z_val} | "
+            f"X_HOME: {x_val} | "
+            f"Y_HOME: {y_val}"
+        )
+
+        time.sleep(0.1)  # 100 ms polling
+
 except KeyboardInterrupt:
     print("\nExit")
 
