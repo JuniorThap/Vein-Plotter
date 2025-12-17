f82b45296d2f610c6b36455578771ef41110f73d
JuniorThap <Thapanawatsuriyaroj@gmail.com>
2025-12-17 13:08:46 +0700

    dkd

--- test/camera_test.py
+++ test/camera_test.py
@@ -3,6 +3,7 @@ from libcamera import controls
 import RPi.GPIO as GPIO
 import cv2
 import time
+from src.vein_selection import build_model, pipeline
 
 # -----------------------------
 # GPIO SETUP
@@ -17,6 +18,7 @@ ir_on = False
 # -----------------------------
 # CAMERA SETUP
 # -----------------------------
+print("Camera Setup")
 picam2 = Picamera2()
 config = picam2.create_preview_configuration(
     main={"format": "RGB888", "size": (640, 480)}
@@ -36,13 +38,15 @@ print("Press 'i' : Toggle IR ON / OFF")
 print("Press 'q' : Quit")
 print("===================================")
 
+model = build_model(program=True)
+
 # -----------------------------
 # MAIN LOOP
 # -----------------------------
 while True:
     frame = picam2.capture_array()
-
     cv2.imshow("Camera Test", frame)
+
     key = cv2.waitKey(1) & 0xFF
 
     if key == ord('i'):
@@ -50,6 +54,13 @@ while True:
         GPIO.output(IR_PIN, GPIO.HIGH if ir_on else GPIO.LOW)
         print(f"[IR] {'ON' if ir_on else 'OFF'}")
         time.sleep(0.3)  # debounce
+    
+    if key == ord('c'):
+        output = pipeline(model, frame, auto=False)
+        plot, lines = cv2.imshow("Output")
+        cv2.imshow(plot)
+        cv2.waitKey(0)
+
 
     if key == ord('q'):
         break
