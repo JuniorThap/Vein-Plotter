2652408345c323d549220815b55deac4f3f07341
JuniorThap <Thapanawatsuriyaroj@gmail.com>
2025-12-17 14:35:36 +0700

    dkd

--- test/motion_test.py
+++ test/motion_test.py
@@ -2,8 +2,11 @@ from src.stepper_motor import Motion2D
 from src.servo_motor import ServoWithLimit
 from src.image_pipeline import Camera
 from src.stepper_calibration import save_calibration
+from src.vein_selection import build_model
+from src.mapping import map_vein_to_motion
 import cv2
 import numpy as np
+import time
 
 cv2.namedWindow("control", cv2.WINDOW_NORMAL)
 cv2.imshow("control", np.zeros((100, 300), dtype=np.uint8))
@@ -13,6 +16,8 @@ print("Click the window. WASD to move, QE for servo, Z to quit")
 motion = Motion2D()
 servo = ServoWithLimit()
 camera = Camera()
+
+model = build_model("", program=True)
 
 
 while True:
@@ -46,6 +51,23 @@ while True:
     elif key == ord('f'):
         x, y = motion.get_position()
         save_calibration(x, y)
+    elif key == ord('1'):
+        print("Detect")
+        vein = camera.detect_vein_points(model, cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
+    elif key == ord('2'):
+        print("Plot first dot")
+        target = map_vein_to_motion(vein, index=0)
+        motion.move_to(target.x_mm, target.y_mm)
+        servo.sweep_until_limit(direction=1)
+        time.sleep(0.1)
+        servo.set_angle(0)
+    elif key == ord('3'):
+        print("Plot second dot")
+        target = map_vein_to_motion(vein, index=1)
+        motion.move_to(target.x_mm, target.y_mm)
+        servo.sweep_until_limit(direction=1)
+        time.sleep(0.1)
+        servo.set_angle(0)
     elif key == ord('z'):
         print("Quit")
         break
