89b4ace542a1d163139414738a2309c241671f80
JuniorThap <Thapanawatsuriyaroj@gmail.com>
2025-12-17 13:15:00 +0700

    dkd

--- main.py
+++ main.py
@@ -16,12 +16,13 @@ import torch
 
 
 def wait_for_button_press():
-    print("Waiting for button press...")
-    while GPIO.input(BUTTON_PIN) == GPIO.HIGH:
-        time.sleep(0.01)
-    print("Button pressed!")
-    while GPIO.input(BUTTON_PIN) == GPIO.LOW:
-        time.sleep(0.01)
+    # print("Waiting for button press...")
+    # while GPIO.input(BUTTON_PIN) == GPIO.HIGH:
+    #     time.sleep(0.01)
+    # print("Button pressed!")
+    # while GPIO.input(BUTTON_PIN) == GPIO.LOW:
+    #     time.sleep(0.01)
+    cv2.waitKey(0)
 
 def reset(motion: Motion2D, servo: ServoWithLimit):
     servo.set_angle(0)
